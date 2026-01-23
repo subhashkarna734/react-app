f69ec6cb38f98160d53f4af0139d8f648ea84f01
Subhash Kumar KARNA <subhash-kumar.karna@sia-partners.com>
2026-01-24 00:56:47 +0530

    'dev'

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+import ReactDOM from 'react-dom/client';
+const heading = React.createElement('h1',{},'Hello World from React!');
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(heading);
